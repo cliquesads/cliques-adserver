fe9e1ec91dcb9f452215910ae900b736b8d3bb1a
Ben Liang <bliang@cliquesads.com>
2015-05-05 13:59:48 -0400

    NF: Setup script to install redis from source, distros in apt-get repos are all super old

---
+++ setup-redis.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#install redis from source to ensure latest version
+REDISVERSION='3.0.1'
+REDISPATH=$HOME"/redis-"$REDISVERSION
+
+if [ ! -d $REDISPATH ]; then
+    cd $HOME
+    wget 'http://download.redis.io/releases/redis-'$REDISVERSION'.tar.gz'
+    tar xzf 'redis-'$REDISVERSION'.tar.gz'
+    cd 'redis-'$REDISVERSION
+    make
+fi
+
+cd $REDISPATH"/src"
+./redis-server $HOME/repositories/cliques-config/redis/redis.conf
--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -3,10 +3,16 @@
 #system deps
 sudo apt-get update
 sudo apt-get install gcc make build-essential
-sudo apt-get install redis-server
+
+#clone config repo and make symlink
+if [ ! -d $HOME"/repositories/cliques-config" ]; then
+    git clone git@github.com:cliquesads/cliques-config.git ../cliques-config
+    ln -s ../cliques-config config
+fi
+
+./setup-redis.sh
 
 #download NVM and install NVM & node
-mkdir
 curl https://raw.githubusercontent.com/creationix/nvm/v0.24.0/install.sh | NVM_DIR=$HOME/repositories/cliques-adserver/.nvm bash
 source .nvm/nvm.sh
 nvm install 0.12.0
@@ -20,10 +26,3 @@ npm install
 #have to install pm2 & mocha globally into nvm dir
 sudo npm install pm2 -g
 sudo npm install mocha -g
-
-#clone config repo and make symlink
-if [ ! -d $HOME"/repositories/cliques-config" ]; then
-    git clone git@github.com:cliquesads/cliques-config.git ../cliques-config
-    ln -s ../cliques-config config
-fi
-
