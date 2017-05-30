a69bf69c9120f7148e392369f80c2866e7800782
Ben Liang <bliang@cliquesads.com>
2017-05-30 15:34:09 -0400

    BF

--- index.js
+++ index.js
@@ -285,7 +285,7 @@ app.get(urls.CLICK_PATH, function(request, response){
                 return;
             }
             if (creative.clickTracker){
-                request.get(creative.clickTracker)
+                request(creative.clickTracker)
                     .on('response', function(response) {
                         console.log(response.statusCode);
                         console.log(response.headers['content-type']);
